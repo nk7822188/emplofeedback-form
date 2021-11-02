3b3d443aaea75ba3e21d1fd023c90a2ef6cd4169
nk7822188 <nk7822188@gmail.com>
2021-11-02 09:48:31 +0530

    3rd assignment

--- src/component/Emfeedback.js
+++ src/component/Emfeedback.js
@@ -5,14 +5,18 @@ export class Container extends Component{
    
     render(){
         return(
+            <div>
             <div className="container">
                 {this.props.entry.map((item)=>{
                     return(
                         <p className="fee" key={item.id}>Name:{item.name} | Department:{item.department} | Rating:{item.rating}</p>
                     )
-                })}
-                <button className="btn1" onClick={()=>this.props.goback()} className="btn">Go back</button> 
+                })}  
                 
+            </div>
+             <div>
+             <button className="btn1" onClick={()=>this.props.goback()} className="btn">Go back</button> 
+            </div>
             </div>
         );
     }
